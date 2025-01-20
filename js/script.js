7b5c451e744d78b1374f6792163a1a0f8fb21b29
Rodrigo Borge <rodrigo@borge.com.br>
2025-01-20 19:09:57 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert("TESTE")
+var nome = prompt('Qual o seu nome?');
+var sobrenome = prompt('Digite seu sobrenome');
+
+console.log('Olá ' + nome + ' ' + sobrenome);
+
+document.write('<h1>Bem vindo ao projeto, ' + nome + '!</h1>');
+document.write('<img src="https://sujeitoprogramador.com/steve.png" alt="Foto do Steve Jobs" />');
