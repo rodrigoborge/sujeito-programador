52946d5a9b232314de63f13a984a3b0356673584
Rodrigo Borge <rodrigo@borge.com.br>
2025-01-21 20:10:16 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,47 @@
-var nome = prompt('Qual o seu nome?');
-var sobrenome = prompt('Digite seu sobrenome');
+// var nome = prompt('Qual o seu nome?');
+// var sobrenome = prompt('Digite seu sobrenome');
 
-console.log('Olá ' + nome + ' ' + sobrenome);
+// console.log('Olá ' + nome + ' ' + sobrenome);
 
-document.write('<h1>Bem vindo ao projeto, ' + nome + '!</h1>');
-document.write('<img src="https://sujeitoprogramador.com/steve.png" alt="Foto do Steve Jobs" />');
+// document.write('<h1>Bem vindo ao projeto, ' + nome + '!</h1>');
+// document.write('<img src="https://sujeitoprogramador.com/steve.png" alt="Foto do Steve Jobs" />');
+
+// Funções no JavaScript
+
+// var area = document.getElementById('area');
+
+// function entrar() {
+//     var nome = prompt('Digite seu nome');
+
+//     if (nome === '' || nome === null) {
+//         alert('Ops! Algo deu errado.');
+//         area.innerHTML = 'Clique no botão para acessar';
+//     } else {
+//         area.innerHTML = 'Bem-vindo, ' + nome + '!';
+
+//         let botaoSair = document.createElement('button');
+//         botaoSair.innerText = 'Sair da Cconta';
+//         botaoSair.onclick = sair;
+//         area.appendChild(botaoSair);
+//     }
+// }
+
+// function sair() {
+//     alert('Até mais...');
+//     area.innerHTML = 'Você saiu!';
+// }
+
+// function mediaAluno(nota1, nota2) {
+//     var media = (nota1 + nota2) / 2;
+
+//     if (media >= 7) {
+//         console.log('Aluno aprovado com a média: ' + media);
+//     } else if (media <= 7) {
+//         console.log('Aluno reprovado pois tirou media: ' + media);
+//     }
+// }
+
+// function aluno(nome, curso) {
+//     var mensagem = 'Seja bem-vindo ' + nome + ' ao curso de ' + curso;
+//     console.log(mensagem);
+// }
