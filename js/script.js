f48937f200ddab9651cadd3aaaaf7c36de220e2f
Rodrigo Borge <rodrigo@borge.com.br>
2025-01-28 22:12:10 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -256,3 +256,15 @@
 
 // REDUCE - Busca reduzir um array
 
+let numeros = [5, 3, 2, 5];
+
+let total = numeros.reduce((acumulador, numero, indice, original) => {
+    console.log(`${acumulador} - total até o momento`);
+    console.log(`${numero} - valor atual`);
+    // console.log(`${indice} - indice atual`);
+    // console.log(`${original} - array original`);
+    console.log('----------******------------');
+    return (acumulador += numero);
+});
+
+console.log(`Total do reduce ${total}`);
