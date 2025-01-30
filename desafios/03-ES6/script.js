eb35ee666fa4b2812ffd2970222d66f0ae6d87ee
Rodrigo Borge <rodrigo@borge.com.br>
2025-01-29 21:29:37 -0300

    Update script.js

---
+++ desafios/03-ES6/script.js
@@ -0,0 +1,20 @@
+// Crie uma lista de produtos
+
+//  A lista produtos deve conter os seguintes produtos: Computador, Telefone, Mouse, Teclado Exiba essa lista no console quando abrir o index.html.
+//  Mostre no console quantos produtos tem nessa lista.
+//  Retire o produto Mouse da lista e retorne no console a lista com os produtos restantes.
+//  Faça uma busca na sua lista por algum produto, por exemplo procure por Computador e caso exista esse produto na sua lista exiba um console falando que este produto existe e mostre tambem o nome deste produto caso não exista mostre uma mensagem avisão que o produto não foi encontrado.
+//  Remova o segundo item da sua lista.
+// Crie uma lista de apenas numeros 1,3,5,7,0,9​
+
+//  Ordene essa lista do menor para o maior.
+//  Retire o primeiro numero desta lista.
+//  Inverta toda ordem da sua lista por exemplo: [1,3,5,7,9] para [9,7,5,3,1]
+// Crie uma string que contenha o dia de hoje, exemplo:
+
+// let hoje = '20/07/2019';
+
+//  Separe essa data em variaveis dia onde contenha apenas o dia, mes onde contenha apenas o mes, e ano onde contenha apenas o ano.
+
+let produtos = ['Computador', 'Telefone', 'Mouse', 'Teclado'];
+console.log(produtos.length);
