de4972eb078065d7b323d9d841791e37cfd7f672
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-04 20:24:18 -0300

    Update script.js

--- reforcos-gus/funcoes/script.js
+++ reforcos-gus/funcoes/script.js
@@ -1,7 +1,18 @@
 //https://www.youtube.com/watch?v=CGotHHt3BFU&list=PLylCwvNCtoak0OiZIdjgN9JSQFwR4Upfv&index=11
 
+//Function padrão
 function soma(num1, num2) {
     return num1 + num2;
 }
 
-console.log(soma(5,2));
+console.log(soma(5, 2));
+
+//Funçao de Seta - Arrow Function
+const soma2 = (num2, num3) => {
+    return num2 + num3;
+};
+console.log(soma2(10, 20));
+
+//Early Return
+const soma3 = (num5, num6) => num5 + num6;
+console.log(soma3(40, 60));
