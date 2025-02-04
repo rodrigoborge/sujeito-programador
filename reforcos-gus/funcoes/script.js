edcec6afae64b26bbb3ba560db5339e105a5da26
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-04 20:06:59 -0300

    Update script.js

--- reforcos-gus/funcoes/script.js
+++ reforcos-gus/funcoes/script.js
@@ -1,2 +1,9 @@
 //https://www.youtube.com/watch?v=CGotHHt3BFU&list=PLylCwvNCtoak0OiZIdjgN9JSQFwR4Upfv&index=11
 
+function soma(num1, num2) {
+    return num1 + num2;
+}
+
+const resultado = soma(10, 20);
+
+console.log(soma(5,2));
