240e07a1f1ebeed48d9a789198595f537c8c39a9
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-03 22:43:50 -0300

    api externa

---
+++ aulas-chaves/consumindo-api-javascript/script.js
@@ -0,0 +1,11 @@
+// URL: https://sujeitoprogramador.com/rn-api/?api=posts
+
+let listElement = document.querySelector('#app');
+
+let posts = [];
+
+function nutriApp() {
+    console.log('Funcionando');
+}
+
+nutriApp();
