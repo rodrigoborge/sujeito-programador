a98b3394e40952f99ec5a19e9c3f428ad22260bc
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-04 20:42:31 -0300

    Update script.js

--- reforcos-gus/funcoes/script.js
+++ reforcos-gus/funcoes/script.js
@@ -18,13 +18,22 @@ const soma3 = (num5, num6) => num5 + num6;
 console.log(soma3(40, 60));
 
 // Funçao que retorna o maior e o menor numero de um array
-const array = [10, 78, 3, 12, 1];
+const arrayNumeros = [10, 78, 3, 12, 1];
 
 function maiorMenorNumero(array) {
     let maior = array[0];
     let menor = array[0];
 
     for (let index = 1; index < array.length; index += 1) {
-        
+        if (array[index] > maior) {
+            maior = array[index];
+        } else {
+            menor = array[index];
+        }
     }
+
+    return [maior, menor];
 }
+//A resposta precisa ser: 78,1
+
+console.log(maiorMenorNumero(arrayNumeros));
