54bf624cca544cbd8d0dd5537f86d4684312f49f
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-03 22:50:51 -0300

    Update script.js

--- aulas-chaves/consumindo-api-javascript/script.js
+++ aulas-chaves/consumindo-api-javascript/script.js
@@ -5,7 +5,18 @@ let listElement = document.querySelector('#app');
 let posts = [];
 
 function nutriApp() {
-    console.log('Funcionando');
+    fetch('https://sujeitoprogramador.com/rn-api/?api=posts')
+        .then((resposta) => resposta.json())
+        .then((json) => {
+            posts = json;
+
+            posts.map((item) => {
+                console.log(item.titulo);
+            });
+        })
+        .catch(() => {
+            console.log('Erro ao buscar dados');
+        });
 }
 
 nutriApp();
