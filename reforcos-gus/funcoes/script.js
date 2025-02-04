637626ebff82ee3a6722bbf3cf6dfe91ba010636
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-04 20:49:14 -0300

    Update script.js

--- reforcos-gus/funcoes/script.js
+++ reforcos-gus/funcoes/script.js
@@ -7,15 +7,21 @@ function soma(num1, num2) {
 
 console.log(soma(5, 2));
 
+
+
 //Funçao de Seta - Arrow Function
 const soma2 = (num2, num3) => {
     return num2 + num3;
 };
 console.log(soma2(10, 20));
 
+
+
 //Early Return
 const soma3 = (num5, num6) => num5 + num6;
 console.log(soma3(40, 60));
+
+
 
 // Funçao que retorna o maior e o menor numero de um array
 const arrayNumeros = [10, 78, 3, 12, 1];
@@ -40,3 +46,5 @@ function maiorMenorNumero(array) {
 
 console.log(maiorMenorNumero(arrayNumeros));
 console.log(maiorMenorNumero(arrayNumeros2));
+
+
