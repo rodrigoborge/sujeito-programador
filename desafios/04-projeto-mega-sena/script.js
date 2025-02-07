e51edab890b24075a952641e25dd099b71991a24
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-06 21:08:43 -0300

    Update script.js

---
+++ desafios/04-projeto-mega-sena/script.js
@@ -0,0 +1,18 @@
+function gerarNumeroMega(qtdNumeros) {
+    if (qtdNumeros < 6 || qtdNumeros > 9) {
+        console.log('Digite apenas numeros de 6 até 9!');
+        return [];
+    }
+
+    const numeros = [];
+
+    while (numeros.length < qtdNumeros) {
+        const numeroAleatorio = Math.floor(Math.random() * 60) + 1;
+        numeros.push(numeroAleatorio);
+    }
+
+    return numeroAleatorio;
+}
+
+const numeroSorteado = gerarNumeroMega(6);
+console.log(numeroSorteado);
